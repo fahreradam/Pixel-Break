65fe0b6b3d0143996f41d810cfb24370c8e0fbcb
fahreradam <72369125+fahreradam@users.noreply.github.com>
2021-03-23 15:16:27 -0400

    Fixed the broken bits

--- Pixel Break/ball.py
+++ Pixel Break/ball.py
@@ -9,5 +9,5 @@ class Ball:
     def draw(self):
         pygame.draw.circle(self.win, (255, 255, 255), self.position, 5)
 
-    def move(self, dt, paddle, target_point):
-
+    def move(self, dt, paddle_pos, target_point):
+        v = [self.position[0] - paddle_pos[0], self.position[1] - paddle_pos[1]]
