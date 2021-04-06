aebbd1e2b84513f81fd9d870f2f191b5f4f3ae78
roguespectator17 <78824261+roguespectator17@users.noreply.github.com>
2021-04-06 14:53:58 -0400

    Fixing....?

--- Pixel Break/main.py
+++ Pixel Break/main.py
@@ -54,19 +54,21 @@ while not done:
     ball.move(dt)
     ball.collision(paddle.position, mPos, paddle.stamina)
     cur_map.render(win, grid_color=None)
-<<<<<<< HEAD
     if attk_exists == True:
         left_attk.update(dt)
         if left_attk.direction != 0:
 
             left_attk.draw(win)
+    else:
+        attk_exists = False
+        attk_timer = 2
+<<<<<<< HEAD
+
 =======
     ball.game_over()
 >>>>>>> 77047267732891280193d2d24015b5ef96914b4e
 
-        else:
-            attk_exists = False
-        attk_timer = 2
+
     pygame.display.flip()
 
 
